9207231b2db732725f0b73279f6bcaafb5a906d8
Tatsuo Ishii <ishii@postgresql.org>
2013-04-15 10:17:31 +0900

    Shell script to create dist files.

---
+++ 2012/evaluation_report/create_dist.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#----------------------------------------------------------------
+# Script to create archive files to be distributed.
+# Currently bz2 and zip are supported.
+#---------------------------------------------------------------- 
+NAME=pgecons-wg1-report
+VERSION=1.0
+FULLNAME=${NAME}-${VERSION}
+TARDIST=${FULLNAME}.tar.bz2
+ZIPDIST=${FULLNAME}.zip
+FILES="README report.odt report.pdf appendix.odt appendix.pdf 2_scaleup"
+tar cf - $FILES|bzip2 -9 -c > $TARDIST
+zip -r $ZIPDIST $FILES
